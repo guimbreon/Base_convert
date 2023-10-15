aff0e74926df1d6f9da8cc70be824d16a7e89ae9
guimbreon <gui.acs26@gmail.com>
2023-10-15 22:24:35 +0100

    Convert to binary v1.0

--- README.md
+++ README.md
@@ -2,42 +2,11 @@
 
 This Python script, `base_transformer.py`, is designed to convert numbers from one base to another. It takes a number in a specified base and converts it to base 10 (decimal). It includes the following functions:
 
-## Functions
+##
 
-### `getData(file_link)`
-
-This function reads data from a specified [file](Keys) and returns the values as a list. Each line in the file is expected to contain a number in a specified base.
-
-- `file_link`: A string representing the path to the file containing the data.
-
-### `getValues()`
-
-This function is used to retrieve a user-defined number and its base for conversion. The values should be formated like this: value|base. Ex: 6712AF16.12414AFCD|16
-
-### `hexaToNums(quote)`
-
-This function converts hexadecimal characters (A-F) in the input to their decimal equivalents (10-15) and returns a list of numbers.
-
-### `convertTo10Whole(quote, base)`
-
-This function converts the whole part of a number (before the decimal point) from the specified base to base 10.
-
-- `quote`: The number in the specified base.
-- `base`: The base of the input number.
-
-### `convertTo10Frac(quote, base)`
-
-This function converts the fractional part of a number (after the decimal point) from the specified base to base 10.
-
-- `quote`: The fractional part of the number in the specified base.
-- `base`: The base of the input number.
-
-### `fromXTo10(quote, base)`
-
-This function combines the conversion of the whole and fractional parts, accounting for the presence of a decimal point in the number. It returns the number in base 10.
-
-- `quote`: The number in the specified base.
-- `base`: The base of the input number.
+With this app you can convert this bases:
+    From 2, 8 and 16 --> 10
+    From 8, 10 and 16 --> 2
 
 ## Example Usage
 
--- converter.py
+++ converter.py
@@ -7,7 +7,8 @@ def getData(file_link):
     return values
 
 def getValues():
-    quote = input("What values do you want to work with?\n(value|base)\n>>>")
+    quote = "429128.12312369|10"
+    #quote = input("What values do you want to work with?\n(value|base)\n>>>")
     quote = quote.split("|")
     return quote[0],int(quote[1])
 
@@ -71,8 +72,73 @@ def fromXTo10(quote,base):
     return new_quote
 
 
-values = getData("CHANGE ME")
+
+def fromXTo2Frac10(quote, base):
+    quote = float("0." + quote)
+
+    new_quote = ""
+    precision = 20  # You can adjust the precision as needed
+
+    for _ in range(precision):
+        quote *= 2
+        new_quote += str(int(quote))
+        quote = quote - int(quote)
+
+
+    return new_quote
+
+
+def fromXTo2All(quote,base):
+    new_quote = ""
+    if base == 10:
+        quote = int(quote)
+        while quote > 0:
+            new_quote += str(quote%2)
+            quote = quote//2
+        return new_quote[::-1]
+    elif base == 16:
+        for item in quote:
+            for hexa in values:
+                if item == hexa[2]:
+                    new_quote += hexa[0]
+        return new_quote
+    elif base == 8:
+        for item in quote:
+            for octa in values:
+                if item == octa[1]:
+                    new_quote += octa[3]
+        return new_quote
+    
+
+    
+
+
+def fromXTo2(quote,base):
+    quote = quote.split(".")
+    print(quote)
+    new_quote = ""
+    if len(quote) != 1:
+        part1 = fromXTo2All(quote[0],base)
+        if base == 16:
+            part2 = fromXTo2Frac10(quote[1],base)
+        else:
+            part2 = fromXTo2All(quote[1],base)
+        new_quote = part1 + "." + part2
+    else:
+        new_quote = fromXTo2All(quote[0],base)
+    return new_quote
+
+
+values = getData("/home/guimbreon/Desktop/git/Base_transformer/Keys")
 quote,base = getValues()
-quoteConvert = fromXTo10(quote,base)
-baseConvert = 10
+#baseConvert = input("What base do you want to transform it too?\n>>>")
+baseConvert = "2"
+if baseConvert == "10":
+    quoteConvert = fromXTo10(quote,base)
+elif baseConvert == "8":
+    quoteConvert = fromXTo8(quote,base)
+elif baseConvert == "2":
+    quoteConvert = fromXTo2(quote,base)
+else:
+    print("The value you inserted is not a possible value to transform to.")
 print(f"The value {quote} that is in the base {base}, converted to {baseConvert} is equal to {quoteConvert}")
